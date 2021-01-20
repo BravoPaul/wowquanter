8faf7b9247722c31b1a1aed20158c32db7609f3e
kunyue <pankun.pk@alibaba-inc.com>
2021-01-20 21:02:53 +0800

    done

--- src/helloworld.py
+++ src/helloworld.py
@@ -6,3 +6,14 @@ auth('18201150441','5513561pkPK')
 
 stocks = get_index_stocks('000300.XSHG')
 print(stocks)
+
+g.stort_in_period = 20
+g.short_out_period = 10
+g.short_init_maxback_thold = 2
+g.short_add_thold = 0.5
+g.short_N = 4
+
+g.long_N = {}
+g.long_days = {}
+g.long_break_price = {}
+g.long_stock = {}
