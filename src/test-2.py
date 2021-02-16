7e69995a662ed96c70030ccf71d4ae2e9b346823
kunyue <pankun.pk@alibaba-inc.com>
2021-02-16 16:51:42 +0800

    30分钟算一次

--- src/test-2.py
+++ src/test-2.py
@@ -1,22 +1,58 @@
 import pandas as pd
+import json
+from datetime import datetime
 
-di = {'haha': ['1', '1', '2', '3'], 'name': [5, 2, 2, 4]}
+bull_stock = pd.read_excel('/Users/kunyue/project_personal/wowquanter/data/bull_stock_2016_2021_new (27).xlsx')
+st_fund_stock = pd.read_excel('/Users/kunyue/project_personal/wowquanter/data/st_result (1).xlsx')
+# st_fund_stock['start_date'] = st_fund_stock['start_date'].map(lambda x: datetime.strptime(x, '%Y-%m-%d'))
+# st_fund_stock['end_date'] = st_fund_stock['end_date'].map(lambda x: datetime.strptime(x, '%Y-%m-%d'))
+st_fund_stock = st_fund_stock.sort_values(by=['code', 'start_date'])
 
-p = pd.DataFrame.from_dict(di)
+result_1_prev = [-1 for i in range(len(bull_stock))]
+result_2_prev = [-1 for i in range(len(bull_stock))]
+result_3_prev = [-1 for i in range(len(bull_stock))]
+result_1_now = [-1 for i in range(len(bull_stock))]
+result_2_now = [-1 for i in range(len(bull_stock))]
+result_3_now = [-1 for i in range(len(bull_stock))]
+result_1_up = [-1 for i in range(len(bull_stock))]
+result_2_up = [-1 for i in range(len(bull_stock))]
+result_3_up = [-1 for i in range(len(bull_stock))]
 
-g = {'1': 0, '2': 3}
+for i, value in bull_stock.iterrows():
+    st_fund_stock_c = st_fund_stock[st_fund_stock['code'] == value['code']]
+    s_raise_range = value['N_3']
+    dict_range = json.loads(s_raise_range)
+    for j, one_range in enumerate(dict_range):
+        if j > 2:
+            break
+        start_date = datetime.strptime(one_range['start_time'], '%Y-%m')
+        end_date = datetime.strptime(one_range['end_time'], '%Y-%m')
+        st_fund_stock_c_tmp = st_fund_stock_c[
+            (st_fund_stock_c['start_date'] > start_date) & (st_fund_stock_c['end_date'] < end_date)].head(1)
 
+        st_fund_stock_c_prev = st_fund_stock_c[st_fund_stock_c['end_date'] < start_date].tail(1)
 
+        if len(st_fund_stock_c_tmp) != 0:
+            if len(st_fund_stock_c_prev) != 0:
+                prev_value = int(st_fund_stock_c_prev['count'].values[0])
+            else:
+                prev_value = 1
+            now_value = int(st_fund_stock_c_tmp['count'].values[0])
 
-def grp_func(x, grp_key):
-    # print(x)
-    # print()
-    # print(x[grp_key][0])
-    if g.get(x[grp_key].values[0]) is not None:
-        g[x[grp_key].values[0]] = g[x[grp_key].values[0]]+sum(x['name'].tolist())
+            globals().get('result_' + str(j + 1) + '_prev')[i] = prev_value
+            globals().get('result_' + str(j + 1) + '_now')[i] = now_value
+            globals().get('result_' + str(j + 1) + '_up')[i] = (now_value - prev_value) / prev_value
 
+bull_stock['1_prev'] = result_1_prev
+bull_stock['1_now'] = result_1_now
+bull_stock['1_up'] = result_1_up
 
-grp_p = p.groupby(['haha']).apply(lambda x:grp_func(x,grp_key='haha'))
+bull_stock['2_prev'] = result_2_prev
+bull_stock['2_now'] = result_2_now
+bull_stock['2_up'] = result_2_up
 
-print(g)
+bull_stock['3_prev'] = result_3_prev
+bull_stock['3_now'] = result_3_now
+bull_stock['3_up'] = result_3_up
 
+bull_stock.to_excel('/Users/kunyue/project_personal/wowquanter/data/bull_2.xlsx')
